7e78f6d5e5a2daf2b88644b3c7790c8deab23735
ndeguine <ndeguine@auchan.net>
2015-11-04 13:41:26 +0100

    Added Price and Currency implementation.

---
+++ src/Model/Currency.php
@@ -0,0 +1,50 @@
+<?php
+
+class Currency
+{
+    /**********************/
+    /* Private properties */
+    /**********************/
+
+    /**
+     * @var string
+     */
+    private $code;
+
+    /*******************/
+    /* Getters-setters */
+    /*******************/
+
+    /**
+     * @param string $code
+     * @return self
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /*******************/
+    /* Public function */
+    /*******************/
+
+    /**
+     * @param Currency $currency
+     *
+     * @return bool
+     */
+    public function isEqualTo(Currency $currency)
+    {
+        return ($this->getCode() === $currency->getCode());
+    }
+}
---
+++ src/Model/Price.php
@@ -0,0 +1,76 @@
+<?php
+
+class Price
+{
+    /**********************/
+    /* Private properties */
+    /**********************/
+
+    /**
+     * @var float
+     */
+    private $amount = 0.0;
+
+    /**
+     * @var Currency
+     */
+    private $currency;
+
+    /*****************/
+    /* Magic methods */
+    /*****************/
+
+    /**
+     * Constructor
+     *
+     * @param $amount
+     * @param Currency $currency
+     */
+    public function __construct($amount, Currency $currency)
+    {
+        $this->amount = (float) $amount;
+        $this->currency = $currency;
+    }
+
+    /*******************/
+    /* Getters-setters */
+    /*******************/
+
+    /**
+     * @return Currency
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /******************/
+    /* Public methods */
+    /******************/
+
+    /**
+     * @param Price $price
+     *
+     * @return Price
+     *
+     * @throws UnexpectedCurrencyException
+     */
+    public function add(Price $price)
+    {
+        if (!$this->currency->isEqualTo($price->getCurrency())) {
+            throw new UnexpectedCurrencyException();
+        }
+
+        $amount = $this->getAmount() + $price->getAmount();
+
+        return new Price($amount, $this->getCurrency());
+    }
+}
